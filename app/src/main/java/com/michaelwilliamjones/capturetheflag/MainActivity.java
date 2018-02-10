7d5d2f1a8725e51e25a4e452911d737a46231a3f
Mike Jones <mwj12@case.edu>
2018-02-10 17:27:10 -0500

    hit fb login callback, check accesstoken status

--- app/src/main/java/com/michaelwilliamjones/capturetheflag/MainActivity.java
+++ app/src/main/java/com/michaelwilliamjones/capturetheflag/MainActivity.java
@@ -1,12 +1,18 @@
 package com.michaelwilliamjones.capturetheflag;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,14 +25,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         callbackManager = CallbackManager.Factory.create();
+        boolean loggedIn = AccessToken.getCurrentAccessToken() == null;
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
 
         //register button click handler
-        registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
                 // load the register activity
                 setContentView(R.layout.activity_registration);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
