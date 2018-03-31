7a09d6a9a8bfedaa6fff209b9f82cedcc5c8fd5a
Mike Jones <mwj12@case.edu>
2018-03-31 01:25:25 -0400

    make registration the main view, have register launch mapsactivity

--- app/src/main/java/com/michaelwilliamjones/capturetheflag/RegistrationActivity.java
+++ app/src/main/java/com/michaelwilliamjones/capturetheflag/RegistrationActivity.java
@@ -1,13 +1,20 @@
 package com.michaelwilliamjones.capturetheflag;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activioty_registration);
+        setContentView(R.layout.activity_registration);
+    }
+
+    public void onRegistrationClick(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
